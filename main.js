c85c46f2cdbdf694129c0ae8b2915a9dab9bb37b
Tejas7545 <76861092+Tejas7545@users.noreply.github.com>
2021-08-25 15:47:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,18 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist>0.2){
+
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimals=floor(InNumberleftWristY);
+    volume=remove_decimals/500;
+    document.getElementById("volume").innerHTML="volume="+volume;
+    song.setVolume(volume);
+    }
+
 }
 function preload(){
     song=loadSound("music one.mp3");
@@ -28,6 +40,8 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
     console.log(results);
+    scoreLeftWrist=results[0].pose.keypoints[9].score;
+    console.log("scoreLeftWrist="+scoreLeftWrist);
     leftWristX=results[0].pose.leftWrist.x;
     leftWristY=results[0].pose.leftWrist.y;
     rightWristX=results[0].pose.rightWrist.x;
